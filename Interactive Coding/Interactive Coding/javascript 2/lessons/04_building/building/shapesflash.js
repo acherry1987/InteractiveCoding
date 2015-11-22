82742d3566116c2381bc641e62255602fa2df117
Andrew Cherry <andrewcherry.v@gmail.com>
2015-11-21 20:33:35 -0500

    Adding the latest Self portrait files

---
+++ Interactive Coding/Interactive Coding/javascript 2/lessons/04_building/building/shapesflash.js
@@ -0,0 +1,38 @@
+(function (lib, img, cjs, ss) {
+
+var p; // shortcut to reference prototypes
+
+// library properties:
+lib.properties = {
+	width: 550,
+	height: 400,
+	fps: 24,
+	color: "#FFFFFF",
+	manifest: []
+};
+
+
+
+// symbols:
+
+
+
+// stage content:
+(lib.shapesflash = function() {
+	this.initialize();
+
+	// Layer 1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#0066CC").s().p("ArrDxQBWlYCVqZIPvgIQCbJ1BiF2IrpImgAAcrJIgFAAIgSACIgTADQhJANg+AnQh4AWhbBbQhGBHgeBYQgUAQgUATQhIBKgdBaQgQAvgDAzQgCARAAARQAAB7BABjQAYAlAiAiQAiAiAnAZQAPApAYAmQAYAmAiAiQA2A2BAAeQBRAnBhAAQA8AAA4gQQA0gPAugdQAqgaAlglIAFgFQAjgEAhgKQA0gOAugdQAqgaAlglQBghgAViAQAFgiAAglQAAgbgDgXQgDgagGgZQgGgcgLgaQgWg1glgvIAIgUIgdgYIgOgfQgDgagGgYQgGgcgLgaQgRgrgaglQgTglgbgiIgHgIQgNgPgOgPQg/g+hLgeQgagLgcgHIgLgDQgsgJgwAAQgSAAgSACg");
+	this.shape.setTransform(190,229.1);
+
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f("#33CCCC").s().p("AOeNzIgMgyIg6ABIiPpLIIolTIChCHIAANugAyhl4QgXgLgWgOQgngZgigiQgigigYgmQhAhiAAh9QAAgRABgRQAEgzAQgvIABgFIALgcIMCAAIADAIQALAaAGAcQAGAYADAaQADAaAAAbQAAAkgGAjQgUB/hhBhQgkAlgqAaQguAcg0APQghAKgjAEQgZACgZAAQhiAAhQgmg");
+	this.shape_1.setTransform(292.3,185.2);
+
+	this.addChild(this.shape_1,this.shape);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(390.1,293.1,319.9,213.8);
+
+})(lib = lib||{}, images = images||{}, createjs = createjs||{}, ss = ss||{});
+var lib, images, createjs, ss;
---
+++ Interactive Coding/Interactive Coding/javascript 2/lessons/04_building/shap36.js
@@ -0,0 +1,34 @@
+(function (lib, img, cjs, ss) {
+
+var p; // shortcut to reference prototypes
+
+// library properties:
+lib.properties = {
+	width: 550,
+	height: 400,
+	fps: 24,
+	color: "#FFFFFF",
+	manifest: []
+};
+
+
+
+// symbols:
+
+
+
+// stage content:
+(lib.shap36 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#33CCCC").s().p("Ao0KwIjfu2INCn7ILlJ/Il7OEg");
+	this.shape.setTransform(162.2,134.1);
+
+	this.addChild(this.shape);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(358.3,257.1,157.7,154.1);
+
+})(lib = lib||{}, images = images||{}, createjs = createjs||{}, ss = ss||{});
+var lib, images, createjs, ss;
